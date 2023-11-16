26a6598f75fbb61ee973b8202c05192d2947dfb6
dondycles <johnrod.dondoyano@gmail.com>
2023-11-16 11:43:58 +0800

    modified:   app/globals.css 	modified:   components/ui/nav.tsx 	modified:   components/ui/nextui.tsx

--- components/ui/nav.tsx
+++ components/ui/nav.tsx
@@ -6,22 +6,34 @@ import logo from "@/public/images/Flyff_2.png";
 import { Button, ButtonGroup } from "@nextui-org/react";
 export default function Nav() {
   return (
-    <nav className="fixed w-[100%] top-0 py-8 flex flex-row justify-between items-center px-4 sm:px-8 pr-16 z-50 bg-foreground/25 ">
-      <Link href="/">
+    <nav className="fixed w-[100%] top-0 py-8 flex flex-row gap-4 justify-between items-center px-4 sm:px-8  z-50 bg-foreground/25 ">
+      <Link href="/" className="">
         <Image src={logo} alt="Flyff" height={114} width={114} />
       </Link>
       <ButtonGroup
         color="primary"
         variant="shadow"
-        className="flex flex-row gap-[1px]"
+        className="flex flex-row gap-[1px] mx-auto sm:mx-0"
       >
-        <Button as={Link} className="font-bold text-white " href="/">
+        <Button
+          as={Link}
+          className="font-bold text-white text-xs sm:text-base min-w-0"
+          href="/"
+        >
           NFT
         </Button>
-        <Button as={Link} className="font-bold text-white " href="/">
+        <Button
+          as={Link}
+          className="font-bold text-white text-xs sm:text-base  min-w-0"
+          href="/"
+        >
           ROADMAP
         </Button>
-        <Button as={Link} className="font-bold text-white " href="/">
+        <Button
+          as={Link}
+          className="font-bold text-white text-xs sm:text-base  min-w-0"
+          href="/"
+        >
           WHITEPAPER
         </Button>
       </ButtonGroup>
--- components/ui/nextui.tsx
+++ components/ui/nextui.tsx
@@ -6,17 +6,15 @@ import CoralIsland_4 from "@/public/images/CoralIsland_4.webp";
 import Image from "next/image";
 export function NextUI({ children }: { children: React.ReactNode }) {
   return (
-    <NextUIProvider className="max-h-[100dvh] h-screen w-full  text-foreground flex flex-col light overflow-auto">
+    <NextUIProvider className="max-h-[100dvh] h-screen w-full  text-foreground light overflow-auto">
       <Nav />
-      <main className="overflow-auto w-full max-h-full h-full">
-        {children}
-        <Image
-          className="fixed top-0 left-0 w-full h-full object-cover z-[-1] brightness-50 "
-          src={CoralIsland_4}
-          alt="CoralIsland"
-          fill
-        />
-      </main>
+      {children}
+      <Image
+        className="fixed top-0 left-0 w-full h-full object-cover z-[-1] brightness-50 "
+        src={CoralIsland_4}
+        alt="CoralIsland"
+        fill
+      />
     </NextUIProvider>
   );
 }
